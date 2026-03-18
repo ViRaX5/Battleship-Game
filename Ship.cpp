ddae53cf3d68211f16d40f25cb69497374eb23dd
ViRaX5 <126521627+ViRaX5@users.noreply.github.com>
2026-03-18 22:55:47 +0200

    Update Ship.cpp

--- Ship.cpp
+++ Ship.cpp
@@ -1,13 +1,7 @@
-// Dor Mandel;      ID : 315313825
-// Amit Lachmann;   ID : 207448267 
-// -------------------------------------------
+
 #include "Ship.hpp"
 #include <iostream>
-// -------------------------------------------
 
-/*
-increments hitsTaken and Prints "<shipname> got hit!"
-*/
 void Ship::takeHit()
 {
   hitsTaken++;
