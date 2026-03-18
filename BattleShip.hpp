f4466ac0cf468629faff3227889e10d7c0619b06
ViRaX5 <126521627+ViRaX5@users.noreply.github.com>
2026-03-18 22:47:44 +0200

    Update BattleShip.hpp

--- BattleShip.hpp
+++ BattleShip.hpp
@@ -1,11 +1,8 @@
-// Dor Mandel;      ID : 315313825
-// Amit Lachmann;   ID : 207448267 
-// -------------------------------------------
+
 #pragma once
-// -------------------------------------------
+
 #include "Ship.hpp"
-// -------------------------------------------
-// BattleShip Size = 4
+
 class BattleShip : public Ship
 {
 public:
