4b4cbd61cfa224fd57528b4476e5a6aa4ba7ebcf
ViRaX5 <126521627+ViRaX5@users.noreply.github.com>
2026-03-18 22:48:01 +0200

    Update Carrier.hpp

--- Carrier.hpp
+++ Carrier.hpp
@@ -1,11 +1,8 @@
-// Dor Mandel;      ID : 315313825
-// Amit Lachmann;   ID : 207448267 
-// -------------------------------------------
+
 #pragma once
-// -------------------------------------------
+
 #include "Ship.hpp"
-// -------------------------------------------
-// Carrier Size = 5
+
 class Carrier :  public Ship
 { 
 public:
