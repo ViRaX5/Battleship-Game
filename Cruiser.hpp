d10f1c248f794430eed898cb21471fceead574d8
ViRaX5 <126521627+ViRaX5@users.noreply.github.com>
2026-03-18 22:48:17 +0200

    Update Cruiser.hpp

--- Cruiser.hpp
+++ Cruiser.hpp
@@ -1,11 +1,8 @@
-// Dor Mandel;      ID : 315313825
-// Amit Lachmann;   ID : 207448267 
-// -------------------------------------------
+
 #pragma once
-// -------------------------------------------
+
 #include "Ship.hpp"
-// -------------------------------------------
-// Cruiser Size = 3
+
 class Cruiser : public Ship
 {
 public:
