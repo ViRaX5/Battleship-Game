30ca81335961e740c0f3df254b2b2de33b998c0e
ViRaX5 <126521627+ViRaX5@users.noreply.github.com>
2026-03-18 22:56:27 +0200

    Update Submarine.hpp

--- Submarine.hpp
+++ Submarine.hpp
@@ -1,11 +1,8 @@
-// Dor Mandel;      ID : 315313825
-// Amit Lachmann;   ID : 207448267 
-// -------------------------------------------
+
 #pragma once
-// -------------------------------------------
+
 #include "Ship.hpp"
-// -------------------------------------------
-// Submarine Size = 3
+
 class Submarine : public Ship
 {
 public:
